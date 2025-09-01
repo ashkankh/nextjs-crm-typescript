158f75d9d4c1306b8c7de9cb54819f461073423c
ashkankh <ashkan1@outlook.com>
2025-09-01 19:20:57 +0330

    feat(Form button): add Form Button

--- components/templates/addCustomerPage.tsx
+++ components/templates/addCustomerPage.tsx
@@ -3,6 +3,16 @@ import Form from '../modules/form'
 import { CustomerForm } from '@/types/form.type'
 
 function AddCustomerPage() {
+
+    function saveHandler() {
+        //   TODO  
+    }
+
+    function cancelHandler() {
+        //   TODO  
+
+    }
+
     const [form, setForm] = useState<CustomerForm>({
         name: "",
         lastName: "",
@@ -18,6 +28,10 @@ function AddCustomerPage() {
         <>
             <div>Add Customer Page</div>
             <Form form={form} setForm={setForm} />
+            <div className='flex flex-row justify-around'>
+                <button className='flex felx-row justify-center py-1 px-4 border-1 font-semibold text-red-500 rounded-sm hover:cursor-pointer' onClick={cancelHandler}>Cancel</button>
+                <button className='flex felx-row justify-center py-1 px-4 border-1 font-semibold text-green-500 rounded-sm hover:cursor-pointer' onClick={saveHandler}>Save</button>
+            </div>
         </>
     )
 }
